0792a096570e5e74b9c2128db5b453706547c10f
penguindoggy <penguinineteen@gmail.com>
2020-12-14 20:42:13 -0800

    init working

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (res, req) => {
-    return res.send('Hello')
+app.get('/', (req, res) => {
+  return res.send('Hello')
 })
 
 app.listen('3000', () => {
-    console,log('Server is running at port 3000')
+  console.log('Server is running at port 3000')
 })
